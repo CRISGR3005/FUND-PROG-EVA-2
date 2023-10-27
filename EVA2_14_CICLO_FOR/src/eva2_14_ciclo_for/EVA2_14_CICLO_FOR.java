260f82511629f3211dbaeb86504455b54c72dad6
CRISGR3005 <143569144+CRISGR3005@users.noreply.github.com>
2023-10-26 19:46:55 -0600

    Add files via upload

---
+++ EVA2_14_CICLO_FOR/src/eva2_14_ciclo_for/EVA2_14_CICLO_FOR.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_14_ciclo_for;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_14_CICLO_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //for --> desde
+        //se ejecuta desde un puto hasta otro
+        //for (punto de inicio;condicion para el termino;manera de avanzar (o retroceder)
+        for (int i = 0; i < 10; i++) {
+            
+        }
+    }
+    
+}
